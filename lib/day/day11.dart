c6ef37830f213f4760a38c3fbe50893d49e973bb
xuyingjun <xuyingjun@yy.com>
2019-07-13 18:12:42 +0800

    day11

--- lib/day/day11.dart
+++ lib/day/day11.dart
@@ -19,7 +19,54 @@ class WeWidget extends StatefulWidget {
   }
 }
 
-class WeWidgetState extends State<WeWidget> {
+class WeWidgetState extends State<WeWidget>
+    with SingleTickerProviderStateMixin {
+  //尝试扩展或实现num时，除int或double之外的任何类型都是编译时错误
+  Animation<Decoration> _animation;
+  AnimationController _controller;
+  Animation _curve;
+
+  Decoration _animationValue;
+  AnimationStatus _state;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _controller = AnimationController(
+      duration: const Duration(milliseconds: 3000),
+      vsync: this,
+    );
+    _curve = CurvedAnimation(parent: _controller, curve: Curves.bounceInOut);
+    _animation = DecorationTween(
+      begin: BoxDecoration(
+        borderRadius: BorderRadius.all(Radius.circular(0.0)),
+        color: Colors.red,
+      ),
+      end: BoxDecoration(
+        borderRadius: BorderRadius.all(Radius.circular(100.0)),
+        color: Colors.green,
+      ),
+    ).animate(_curve)
+      ..addListener(() {
+        setState(() {
+          _animationValue = _animation.value;
+        });
+      })
+      ..addStatusListener((AnimationStatus state) {
+        if (state == AnimationStatus.completed) {
+          _controller.reverse();
+        } else if (state == AnimationStatus.dismissed) {
+          _controller.forward();
+        }
+
+        setState(() {
+          _state = state;
+        });
+      });
+    _controller.forward();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -32,9 +79,42 @@ class WeWidgetState extends State<WeWidget> {
 
   Widget _buildColumn() {
     return Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      crossAxisAlignment: CrossAxisAlignment.center,
-      children: <Widget>[],
+      children: <Widget>[
+        AnimatorTransition(
+          child: FlutterLogo(
+            style: FlutterLogoStyle.horizontal,
+            size: 200,
+          ),
+          animation: _animation,
+        ),
+        Text("动画值：" + _animationValue.toStringShort()),
+        Text("动画状态：" + _state.toString()),
+      ],
+    );
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+}
+
+class AnimatorTransition extends StatelessWidget {
+  final Widget child;
+  final Animation<Decoration> animation;
+
+  AnimatorTransition({this.child, this.animation});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: DecoratedBoxTransition(
+        decoration: animation,
+        child: Container(
+          child: this.child,
+        ),
+      ),
     );
   }
 }
