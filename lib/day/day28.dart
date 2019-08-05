f158ce97c50ab3e5046b68b8bedb995fc9aad603
xuyingjun <xuyingjun@yy.com>
2019-08-05 17:41:17 +0800

    day28

--- lib/day/day28.dart
+++ lib/day/day28.dart
@@ -34,7 +34,16 @@ class WeWidgetState extends State<WeWidget> {
     return Column(
       mainAxisAlignment: MainAxisAlignment.center,
       crossAxisAlignment: CrossAxisAlignment.center,
-      children: <Widget>[],
+      children: <Widget>[
+        Padding(
+          padding: EdgeInsets.all(8.0),
+          child: Container(
+            height: 200,
+            width: 200,
+            color: Colors.blue[200],
+          ),
+        )
+      ],
     );
   }
 }
