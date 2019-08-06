0712051f42a3b9fd0c99919620ca13736f6e4aa2
xuyingjun <xuyingjun@yy.com>
2019-08-06 14:43:42 +0800

    day34

--- lib/day/day34.dart
+++ lib/day/day34.dart
@@ -31,10 +31,34 @@ class WeWidgetState extends State<WeWidget> {
   }
 
   Widget _buildColumn() {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      crossAxisAlignment: CrossAxisAlignment.center,
-      children: <Widget>[],
+    //如果child有baseline，则根据child的baseline属性，调整child的位置
+    //如果child没有baseline，则根据child的bottom，来调整child的位置
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: <Widget>[
+        Baseline(
+          //alphabetic：对齐字符底部的水平线
+          //ideographic：对齐表意字符的水平线
+          baselineType: TextBaseline.alphabetic,
+          child: Container(
+            width: 100,
+            height: 100,
+            color: Colors.blue[200],
+            child: Text("Hello World"),
+          ),
+          baseline: 50.0,
+        ),
+        Baseline(
+          baselineType: TextBaseline.alphabetic,
+          child: Container(
+            width: 100,
+            height: 100,
+            color: Colors.blue[600],
+            child: Text("Hello World"),
+          ),
+          baseline: 100.0,
+        ),
+      ],
     );
   }
 }
